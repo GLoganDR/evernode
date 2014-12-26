243047d100c79e63f167a92affa9fea803e6aa55
Logan Richardson <g.logan.richardson@gmail.com>
2014-12-25 22:11:47 -0600

    fixed testing

--- test/e2e/authentication/register.spec.js
+++ test/e2e/authentication/register.spec.js
@@ -1,19 +1,14 @@
 'use strict';
 
-var cp = require('child_process'),
-    h  = require('../../helpers/helpers'),
-    db = h.getdb();
+var h = require('../../helpers/helpers');
 
 describe('register', function(){
-  beforeEach(function(done){
-    cp.execFile(__dirname + '/../../scripts/clean-db.sh', [db], {cwd:__dirname + '/../../scripts'}, function(err, stdout, stderr){
-      browser.get('/#/register');
-      done();
-    });
+  beforeEach(function(){
+    browser.get('/#/register');
   });
 
   it('should get register page', function(){
-    expect(element(by.css('div[ui-view] > h1')).getText()).toEqual('REGISTER');
+    expect(element(by.css('div[ui-view] > h1')).getText()).toEqual('register');
   });
 
   it('should register a new user', function(){
@@ -21,6 +16,6 @@ describe('register', function(){
     element(by.model('user.password')).sendKeys('456');
     element(by.model('user.avatar')).sendKeys('http://images.apple.com/global/elements/flags/16x16/usa_2x.png');
     element(by.css('button[ng-click]')).click();
-    expect(element(by.css('div[ui-view] > h1')).getText()).toEqual('LOGIN');
+    expect(element(by.css('div[ui-view] > h1')).getText()).toEqual('login');
   });
 });
--- test/e2e/notes/notes_list.spec.js
+++ test/e2e/notes/notes_list.spec.js
@@ -1,25 +1,18 @@
 'use strict';
 
-var cp   = require('child_process'),
-    h    = require('../../helpers/helpers'),
-    path = require('path'),
-    db   = h.getdb();
+var path = require('path');
 
-describe('login', function(){
-  beforeEach(function(done){
-    cp.execFile(__dirname + '/../../scripts/clean-db.sh', [db], {cwd:__dirname + '/../../scripts'}, function(err, stdout, stderr){
-      login();
-      done();
-    });
+describe('notes list', function(){
+  beforeEach(function(){
+    login();
   });
 
-  it('should get the notes page', function(){
+  it('should get notes page', function(){
     expect(element(by.css('div[ui-view] > h1')).getText()).toEqual('notes');
   });
 
-  it('should create a new note', function(){
+  it('should create a note', function(){
     create('a', 'b', 'c,d,e');
-
     expect(element(by.model('note.title')).getAttribute('value')).toEqual('');
     expect(element(by.model('note.body')).getAttribute('value')).toEqual('');
     expect(element(by.model('note.tags')).getAttribute('value')).toEqual('');
@@ -27,20 +20,19 @@ describe('login', function(){
   });
 
   it('should go to note detail', function(){
-    create('x', 'y', 'z,aa,bb');
-
+    create('a', 'b', 'c,d,e');
     element(by.repeater('note in notes').row(0)).element(by.css('td:nth-child(2) > a')).click();
-    expect(element(by.css('div[ui-view] > h1')).getText()).toEqual('x');
+    expect(element(by.css('div[ui-view] > h1')).getText()).toEqual('a');
   });
 });
 
 function create(title, body, tags){
   var image = path.resolve(__dirname, '../../fixtures/flag.png');
-    element(by.model('note.title')).sendKeys(title);
-    element(by.model('note.body')).sendKeys(body);
-    element(by.model('note.tags')).sendKeys(tags);
-    element(by.css('input[type="file"]')).sendKeys(image);
-    element(by.css('button[ng-click]')).click();
+  element(by.model('note.title')).sendKeys(title);
+  element(by.model('note.body')).sendKeys(body);
+  element(by.model('note.tags')).sendKeys(tags);
+  element(by.css('input[type="file"]')).sendKeys(image);
+  element(by.css('button[ng-click]')).click();
 }
 
 function login(){
